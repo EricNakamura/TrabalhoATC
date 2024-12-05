28f348191c076cad948cdc11c09093b897e9ec69
EricNakamura <eric.bafa@epicje.pt>
2024-12-05 00:39:26 +0000

    Atualizando a linkagem

---
+++ src/TrabalhoPacMan.cpp
@@ -0,0 +1,13 @@
+﻿// TrabalhoPacMan.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "TrabalhoPacMan.h"
+#include <curses.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
